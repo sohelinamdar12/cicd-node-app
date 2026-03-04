c243158bab10170558d17479b6a95ecc6a139698
Ubuntu <ubuntu@ip-172-31-3-18.ap-south-1.compute.internal>
2026-03-04 18:26:02 +0000

    Initial commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.write("Hello Sohel, you have completed you Lab by self made CICD Pipeline");
+  res.write("I Love you Saniya");
   res.end();
 });
 
